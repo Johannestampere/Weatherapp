1672362e1079893169cd4b878252565b18116ed9
Johannes Tampere <tamperejohannes@gmail.com>
2025-07-20 05:42:07 -0400

    Center div.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -26,7 +26,7 @@ export default function LandingPage() {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
       viewport={{ once: false }}
-      className="z-50 flex w-full flex-col items-start gap-8 px-4 pt-32 pb-48 text-md md:w-3/4 lg:w-1/2 justify-center items-center gap-10 min-h-screen"
+      className="flex flex-col items-center justify-center w-full min-h-screen gap-10 px-4 pt-32 pb-48 text-md md:w-3/4 lg:w-1/2 z-50"
     >
       <h1 className="text-5xl font-extrabold text-green-900 drop-shadow-lg text-center">Welcome to <span className="text-green-700">WeatherGPT</span>,<br/> sign in to get started</h1>
       <div>
